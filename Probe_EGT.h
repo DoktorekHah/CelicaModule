a0ca12a080760693473fa56754397178b8b1028a
DoktorekHah <lukasz.grabski660@gmail.com>
2022-03-13 22:28:54 +0100

    Dodanie pliku Probe_EGT.cpp z funkcjami

--- Probe_EGT.h
+++ Probe_EGT.h
@@ -1,51 +1,17 @@
+#ifndef PROBE_EGT_H
+#define PROBE_EGT_H
+
 #include "Configuration.h"
 #include "Display_OLED.h"
 
-float reading, voltage, temperature;
+float get_voltage(int raw_adc);
 
-float get_voltage(int raw_adc) {
-  return raw_adc * (AREF / (pow(2, ADC_RESOLUTION)-1));  
-}
+float get_temperature(float voltage);
 
-float get_temperature(float voltage) {
-  return (voltage - 1.25) / 0.005;
-}
+void egt_read_cyl_1();
 
-void egt_read_cyl_1() {
-  reading = analogRead(PIN_A10_EGT_CYL1);
-  voltage = get_voltage(reading);
-  temperature = get_temperature(voltage);
-  //Display_Write_EGT(temperature, DELAY_OLED, TEXT_SIZE_4);
-  #ifdef DEBUG_EGT
-    Serial.print("Temperature = ");
-    Serial.print(temperature);
-    Serial.println(" C");
-  #endif
-  delay(500);
-}
+void egt_read_cyl_2();
 
-void egt_read_cyl_2() {
-  reading = analogRead(PIN_A8_EGT_CYL2);
-  voltage = get_voltage(reading);
-  temperature = get_temperature(voltage);
- // Display_Write_EGT(temperature, DELAY_OLED, TEXT_SIZE_4);
-  #ifdef DEBUG_EGT
-    Serial.print("Temperature = ");
-    Serial.print(temperature);
-    Serial.println(" C");
-  #endif
-  delay(500);
-}
+void egt_read_cyl_4();
 
-void egt_read_cyl_4() {
-  reading = analogRead(PIN_A6_EGT_CYL4);
-  voltage = get_voltage(reading);
-  temperature = get_temperature(voltage);
- // Display_Write_EGT(temperature, DELAY_OLED, TEXT_SIZE_4);
-  #ifdef DEBUG_EGT
-    Serial.print("Temperature = ");
-    Serial.print(temperature);
-    Serial.println(" C");
-  #endif
-  delay(500);
-}
+#endif
